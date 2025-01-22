e5d607d2e3f78de88df47873fba6d9456118f84e
Ariq Aziz <muhaariq@gmail.com>
2025-01-22 15:56:35 +0700

    feat: middleware jwt and logout

--- app/repository/user_repository.go
+++ app/repository/user_repository.go
@@ -20,6 +20,19 @@ func GetUserByUsername(ctx context.Context, username string) (models.User, error
 	return resp, err
 }
 
+func GetUserSessionByToken(ctx context.Context, token string) (models.UserSession, error) {
+	var (
+		resp models.UserSession
+		err  error
+	)
+	err = database.DB.Where("token = ?", token).Last(&resp).Error
+	return resp, err
+}
+
 func InsertNewUserSession(ctx context.Context, session *models.UserSession) error {
 	return database.DB.Create(session).Error
+}
+
+func DeleteUserSessionByToken(ctx context.Context, token string) error {
+	return database.DB.Exec("DELETE FROM user_sessions WHERE token = ?", token).Error
 }
